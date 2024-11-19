f4c18d691dd3dd6ce9cb30704865b82a1bc0d5d6
Kyo-XZ <ms.noctis@gmail.com>
2024-11-19 16:45:35 +0100

    Suite du travail sur la page checkout

--- mystore/shop/views/checkout_view.py
+++ mystore/shop/views/checkout_view.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from shop.services.cart_service import CartService
 from shop.models.Carrier import Carrier
 from shop.forms.CheckoutAddressForm import CheckoutAddressForm
+from django.contrib import messages
 
 
 def index(request):
@@ -24,3 +25,20 @@ def index(request):
         'carriers' : carriers,
         'address_form' : address_form,
         })
+
+def add_address(request):
+    user = request.user
+    
+    #Vérification de la méthode
+    if request.method == 'POST' :
+        #Récupération du formulaire
+        address_form = CheckoutAddressForm(request.POST)
+        #Vérification de la validité
+        if address_form.is_valid():
+            address = address_form.save(commit=False) #sauvegarde sans envois en base de donné
+            address.author = user #Association de l'user à author
+            address.save()
+            messages.success(request, 'Adresse correctement ajoutée.')
+
+
+    return redirect('shop:checkout')
